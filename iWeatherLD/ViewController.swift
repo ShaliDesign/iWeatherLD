d8568ddeda977b323b84cfcca1e85fbbe04c010e
Linh&ShawnD <linhshawn-d@LinhShawnDs-MacBook-Pro.local>
2017-01-26 09:29:01 -0800

    Initial Commit

---
+++ iWeatherLD/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iWeatherLD
+//
+//  Created by Linh&ShawnD on 2017-01-26.
+//  Copyright © 2017 Shali Design. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
